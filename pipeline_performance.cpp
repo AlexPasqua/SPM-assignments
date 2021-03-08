6d158b863c4659f9d17c63d4280531a1e53edb57
AlexPasqua <alexpasquali98@gmail.com>
2021-03-08 17:15:13 +0100

    Created apposit file for myqueue

--- pipeline_performance.cpp
+++ pipeline_performance.cpp
@@ -10,35 +10,7 @@
 #include <functional>
 #include <thread>
 
-// The queue code itself. It is defined as a template such that we can defined queues with different item types:
-template <typename T>
-class myqueue
-{
-private:
-  std::mutex              d_mutex;
-  std::condition_variable d_condition;
-  std::deque<T>           d_queue;
-public:
-
-  myqueue(std::string s) { std::cout << "Created " << s << " queue " << std::endl;  }
-  myqueue() {}
-  
-  void push(T const& value) {
-    {
-      std::unique_lock<std::mutex> lock(d_mutex);
-      d_queue.push_front(value);
-    }
-    this->d_condition.notify_one();
-  }
-  
-  T pop() {
-    std::unique_lock<std::mutex> lock(d_mutex);
-    d_condition.wait(lock, [=]{ return !d_queue.empty(); });  // wait if the queue is currently empty
-    T rc(std::move(this->d_queue.back()));       // used to get the actual message rather than a copy
-    d_queue.pop_back();                   // remove the item just read from the back end of the queue
-    return rc;
-  }
-};
+#include "myqueue.cpp"
 
 // definition of end of stream
 #define EOS -1
@@ -156,7 +128,7 @@ int main(){
   }
   
   cout << "Theoretical time: " << \
-    completionTime(taskNo, msecs_src, msecs_s1, msecs_s2, msecs_drn).count() << '\n';
+    completionTime(taskNo, msecs_src, msecs_s1, msecs_s2, msecs_drn).count() << " mesc\n";
 
   return 0;
 }
